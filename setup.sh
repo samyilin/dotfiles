81a25ad9b1912bead03e2fd4654f5baa891ff846
samyilin <36393679+samyilin@users.noreply.github.com>
2022-10-01 23:57:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,42 +3,27 @@
 # Sets up bash profile, vimrc and more. WIP
 # Target endstate: Have some flags to get the config files, and some hints/help the user to set up their system, hopefully.
 # Right now only CLI is considered here. If needed, can try to add some GUI setup here?
+# quit script if error occurs
 set -e
 # go into home dir, if not already
 cd ~
-# copy bashrc and bash aliases in. 
-# If argument --bash_import_method (-b) equals 'append', imports bashrc in by checking for existence and appending it to its end if there's an existent bashrc. 
-# This is the default behaviour.
-# If argument --bash_import_method (-b) equals 'separate', check if there's an existing bashrc/bash_aliases, if there is, then add a line at the end to source this file as well.
-# If argument --bash_import_method (-b) equals 'truncate', imports bashrc in by truncating .bashrc and .bash_aliases and importing files within this one as new one
-# If argument --bash_import_method (-b) equals 'link', soft link bashrc and bash_aliases in before copying and preserving existent bashrc and bash_aliases, if any.
-# Copy vimrc in.
-# If Vim (not vi) is not installed, echo some message and quit
-# If argument --vim_import_method (-v) equals 'no', do not import vim setup. 
-# If argument --vim_import_method (-v) equals 'minimal', import vim-sensible's vim-sensible.vim and put that into home directory. Echo user to use this one (TBD)
-# If argument --vim_import_method (-v) equals 'full', import everything from this setup and prompt user how to use vimrc.
-# This is the default behaviour.
-# If argument --vim_import_method (-v) equals 'IDE', get LSP and other goodies in there before making the user aware of it. Prompts the user that they could use Neovim, Emacs or VSCode as well.
-if [ -f ~/.bashrc ]; then
-    if ! grep -FxPq "if [ -f ~/.bash_personal ]; then . ~/.bash_personal; fi" ~/.bashrc; then
-        echo "if [ -f ~/.bash_personal ]; then . ~/.bash_personal; fi" >> ~/.bashrc && cp ~/dotfiles/.bashrc ~/.bash_personal
-    fi
-fi
-else
+# copy bashrc and bash aliases in.
+echo "This program is not meant to be used repetitively."
+echo "if some programs weren't installed during initial setup, go to this repository's "
+echo "subfolder with the programs' name to execute the appropriate setup file."
+touch .profile && mv -f .profile .profile.bak
+touch .bashrc && mv -f .bashrc .bashrc.bak
 
-if [ -f ~/.bash_aliases ]; then
-    if ! grep -Fxq "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" ~/.bashrc; 
-        echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" >> ~/.bashrc && cp ~/dotfiles/.bash_aliases ~/.bash_aliases
-    fi
-fi
-if [ -z "$(which vim)" -o -z "$(which vimtutor)" ]; then
-  echo "Warning, doesn't look like you have full Vim version."
-  echo "Use your system package manager to install vim."
-fi
+ln -sf "$PWD/.profile" "$HOME/.profile"
+ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
+
+for i in vim lynx gh git tmux docker podman iam; do
+  cd $i && ./setup
+  cd -
+done
+
 if ! command -v tmux &> /dev/null; then
-    echo 'tmux is not installed, do you wish to install it? Not installing it would '
-    # prompt the user to say yes or no, then guess which package manager the user needs
-    sudo dnf install tmux || sudo apt-get install 
+    echo 'tmux is not installed, quitting'
 else
     cp dotfiles/.tmux.conf ~/.tmux.conf
 fi
