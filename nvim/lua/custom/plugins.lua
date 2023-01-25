ee00854b0e21aa6bc90410165c82463a246806df
Sam Su <samsu42@yahoo.ca>
2023-01-25 01:33:25 -0500

    Modified settings, added nvim config using kickstart.lua

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+return function(use)
+  use{
+    "tpope/vim-vinegar",
+    config = function()
+    end
+  }
+  use{
+    "sainnhe/gruvbox-material",
+    config = function()
+    end
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+    end
+  }
+  use {
+    'tpope/vim-commentary',
+    config = function()
+    end
+  }
+end
