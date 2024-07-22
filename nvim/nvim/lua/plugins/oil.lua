d4aaddef0bb53da90c3ed16238328b2a0cc4d888
samyilin <samsu42@yahoo.ca>
2024-07-22 15:40:54 -0400

    Dadbod + oil fix
    Fix for dadbod and oil

--- nvim/nvim/lua/plugins/oil.lua
+++ nvim/nvim/lua/plugins/oil.lua
@@ -84,14 +84,14 @@ return {
       use_default_keymaps = true,
       view_options = {
         -- Show files and directories that start with "."
-        show_hidden = false,
+        show_hidden = true,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
           return vim.startswith(name, ".")
         end,
         -- This function defines what will never be shown, even when `show_hidden` is set
         is_always_hidden = function(name, bufnr)
-          return false
+          return vim.startswith(name, "..")
         end,
         -- Sort file names in a more intuitive order for humans. Is less performant,
         -- so you may want to set to false if you work with large directories.
