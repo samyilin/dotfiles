022e0dd15c9e308860b9c6a4b38515d151c1e330
samyilin <samsu42@yahoo.ca>
2024-10-28 11:37:23 -0400

    Windows fuckery lol
    Yeah, windows lol

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,5 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-if vim.fn.filereadable("~/.vimrc") then
+--Loads vimrc for compatibility and comfort. Learned from rwxrob
+if vim.uv.os_uname().sysname == "Windows_NT" and vim.fn.filereadable("~\\_vimrc") then
+  vim.cmd("source ~\\_vimrc")
+elseif vim.fn.filereadable("~/.vimrc") then
   vim.cmd("source ~/.vimrc")
 end
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
