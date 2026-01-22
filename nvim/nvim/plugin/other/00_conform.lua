390cde35d9db03063dec559c24ebea195f33b49e
samyilin <yilinwu@Yilins-MacBook-Air.local>
2026-01-21 21:28:46 -0800

    markdown linting

--- nvim/nvim/plugin/other/00_conform.lua
+++ nvim/nvim/plugin/other/00_conform.lua
@@ -5,6 +5,8 @@ require('conform').setup({
     lua = { 'stylua' },
     sh = { 'shfmt' },
     bash = { 'shfmt' },
+    ['markdown'] = { 'markdownlint-cli2' },
+    ['markdown.mdx'] = { 'markdownlint-cli2' },
   },
   -- Automatically turns on format on save via this setting.
   format_on_save = {
@@ -12,9 +14,25 @@ require('conform').setup({
     timeout_ms = 50000,
     lsp_format = 'fallback',
   },
+  formatters = {
+    -- ['markdown-toc'] = {
+    --   condition = function(_, ctx)
+    --     for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+    --       if line:find('<!%-%- toc %-%->') then return true end
+    --     end
+    --     return false
+    --   end,
+    -- },
+    ['markdownlint-cli2'] = {
+      condition = function(_, ctx)
+        local diag = vim.tbl_filter(function(d) return d.source == 'markdownlint' end, vim.diagnostic.get(ctx.buf))
+        return #diag > 0
+      end,
+    },
+    ['shfmt'] = {
+      append_args = { '-i', '2' },
+      -- The base args are { "-filename", "$FILENAME" } so the final args will be
+      -- { "-filename", "$FILENAME", "-i", "2" }
+    },
+  },
 })
-require('conform').formatters.shfmt = {
-  append_args = { '-i', '2' },
-  -- The base args are { "-filename", "$FILENAME" } so the final args will be
-  -- { "-filename", "$FILENAME", "-i", "2" }
-}
