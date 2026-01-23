ba2781f0b75d3a117f543e276943c53733c082a4
samyilin <yilinwu@Yilins-MacBook-Air.local>
2026-01-22 21:09:38 -0800

    lua_ls adjustment, new keymap

--- nvim/nvim/plugin/core/00_keymaps.lua
+++ nvim/nvim/plugin/core/00_keymaps.lua
@@ -1,5 +1,10 @@
-vim.keymap.set('n', '<leader>l', function() vim.pack.update() end)
+vim.keymap.set('n', '<leader>pu', function() vim.pack.update() end)
+vim.keymap.set('n', '<leader>ps', function() vim.pack.update({}, { target = 'lockfile' }) end)
 -- keymap to jump to neovim config, home dir and cwd.
 vim.keymap.set('n', '<leader>g1', function() vim.cmd(':e ' .. vim.fn.stdpath('config')) end)
 vim.keymap.set('n', '<leader>g2', function() vim.cmd(':e ' .. os.getenv('HOME')) end)
 vim.keymap.set('n', '<leader>g3', function() vim.cmd(':e ' .. vim.uv.cwd()) end)
+vim.keymap.set({ 'i', 'n', 's' }, '<esc>', function()
+  vim.cmd('noh')
+  return '<esc>'
+end, { expr = true })
--- nvim/nvim/plugin/other/00_lazydev.lua
+++ nvim/nvim/plugin/other/00_lazydev.lua
@@ -1,2 +1,4 @@
 vim.pack.add({ 'https://github.com/folke/lazydev.nvim' }, { load = true })
-require('lazydev').setup()
+require('lazydev').setup({ library = {
+  { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+} })
