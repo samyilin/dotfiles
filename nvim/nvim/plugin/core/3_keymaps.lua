e2e4b9a323086786e17de2e001c50eb94513a2a7
samyilin <yilinwu@Yilins-MacBook-Air.local>
2026-01-26 01:24:03 -0800

    fix

--- nvim/nvim/plugin/core/3_keymaps.lua
+++ nvim/nvim/plugin/core/3_keymaps.lua
@@ -1,7 +1,2 @@
 vim.keymap.set('n', '<leader>pu', function() vim.pack.update() end)
 vim.keymap.set('n', '<leader>ps', function() vim.pack.update({}, { target = 'lockfile' }) end)
--- keymap to jump to neovim config, home dir and cwd.
-vim.keymap.set({ 'i', 'n', 's' }, '<esc>', function()
-  vim.cmd('noh')
-  return '<esc>'
-end, { expr = true })
