d1bc22415d1d3fd1f3fd52a5152518165ebef3be
samyilin <yilinwu@Yilins-MacBook-Air.local>
2026-01-31 22:22:10 -0800

    add objects to init.lua

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -19,3 +19,20 @@ Config.later = MiniDeps.later
 Config.now_if_args = vim.fn.argc(-1) > 0 and MiniDeps.now or MiniDeps.later
 
 vim.cmd.colorscheme('minicyan')
+-- Helper for creating a new autocommand
+Config.custom_group = vim.api.nvim_create_augroup('custom-config', {})
+Config.new_autocmd = function(event, opts)
+  opts.group = opts.group or Config.custom_group
+  vim.api.nvim_create_autocmd(event, opts)
+end
+-- load mini.basics and mini.misc immediately
+require('mini.basics').setup({
+  options = {
+    extra_ui = true,
+  },
+})
+require('mini.misc').setup()
+-- Gets rid of the 'border' around Neovim using OSC111
+MiniMisc.setup_termbg_sync()
+-- Restore cursor position
+MiniMisc.setup_restore_cursor()
