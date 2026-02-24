e050a3e8ec3692d205a5062b10a7737ed21ea9bd
samyilin <yilinwu@Yilins-MacBook-Air.local>
2026-02-24 15:44:41 -0800

    fix

---
+++ nvim/nvim/plugin/core/1_autocommands.lua
@@ -0,0 +1,8 @@
+-- resize splits if window got resized
+Config.new_autocmd('VimResized', {
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd('tabdo wincmd =')
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
