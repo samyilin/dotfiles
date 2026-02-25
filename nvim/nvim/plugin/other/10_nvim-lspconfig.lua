2e4d07c20cf2f8d9e63abb38b5c62a3c1e918195
sam.su <sam.su@loblaw.ca>
2026-02-25 15:56:43 -0500

    update update_repos logic

--- nvim/nvim/plugin/other/10_nvim-lspconfig.lua
+++ nvim/nvim/plugin/other/10_nvim-lspconfig.lua
@@ -1,4 +1,6 @@
 Config.later(function()
   vim.pack.add({ { src = 'https://github.com/neovim/nvim-lspconfig' } })
-  vim.lsp.enable({ 'lua_ls' })
+  vim.lsp.enable({
+    vim.fn.executable('lua-language-server') and 'lua_ls' or nil,
+  })
 end)
