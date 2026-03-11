00061e971cd91b1ad3a23545b28579eb02b641ed
sam.su <sam.su@loblaw.ca>
2026-03-11 18:17:27 -0400

    minor tweak to emacs install documentation.

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -3,4 +3,5 @@
 link_config "$PWD/.emacs.d" "$HOME/.emacs.d"
 if [ "$(uname -s)" = "Darwin" ] && has brew; then
   link_config "$PWD/emacs-plus" "$HOME/.config/emacs-plus"
+  printf "Emacs plus patch installec, consider reinstalling emacs plus. \n"
 fi
