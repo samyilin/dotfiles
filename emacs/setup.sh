9b9b4b5821906544d0c8016ea41d522cc1d00627
sam.su <sam.su@loblaw.ca>
2026-03-11 20:46:17 -0400

    fix typo

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -3,5 +3,5 @@
 link_config "$PWD/.emacs.d" "$HOME/.emacs.d"
 if [ "$(uname -s)" = "Darwin" ] && has brew; then
   link_config "$PWD/emacs-plus" "$HOME/.config/emacs-plus"
-  printf "Emacs plus patch installec, consider reinstalling emacs plus. \n"
+  printf "Emacs plus patch installed, consider reinstalling emacs plus. \n"
 fi
