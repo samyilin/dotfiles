e7f10d7c1c8b7ace0148a4b07c95b34e5b871585
sam su <36393679+samyilin@users.noreply.github.com>
2026-03-23 11:58:12 -0700

    better bootstrapping

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -69,19 +69,3 @@ vim.filetype.add({
     mdc = 'markdown', -- Treat .mdc files as markdown
   },
 })
-
--- Bootstrap with mini
-vim.pack.add(
-  { { src = 'https://github.com/nvim-mini/mini.nvim' } },
-  { load = true }
-)
-
--- Define lazy helpers
-local misc = require('mini.misc')
-Config.now = function(f) misc.safely('now', f) end
-Config.later = function(f) misc.safely('later', f) end
-Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
-Config.on_event = function(ev, f) misc.safely('event:' .. ev, f) end
-Config.on_filetype = function(ft, f) misc.safely('filetype:' .. ft, f) end
-
-vim.cmd.colorscheme('miniautumn')
---
+++ nvim/nvim/plugin/other/20_final_touches.lua
@@ -0,0 +1,2 @@
+-- Putting configs that need multiple plugins here for final touch.
+vim.cmd.colorscheme('miniautumn')
