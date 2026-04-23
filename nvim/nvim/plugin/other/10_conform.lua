f0cc05ac5ca9d3d67c78db766bd4f2ae5d45397a
sam su <36393679+samyilin@users.noreply.github.com>
2026-04-23 01:14:08 -0700

    conform messages, haven't figured it out

--- nvim/nvim/plugin/other/10_conform.lua
+++ nvim/nvim/plugin/other/10_conform.lua
@@ -51,11 +51,6 @@ Config.later(function()
   vim.api.nvim_create_user_command('FormatToggle', function()
     vim.b.disable_autoformat = not vim.b.disable_autoformat
     vim.g.disable_autoformat = not vim.g.disable_autoformat
-    if vim.b.disable_autoformat then
-      vim.notify('Autoformat disabled')
-    else
-      vim.notify('Autoformat enabled')
-    end
   end, {
     desc = 'Toggle autoformat on save',
   })
