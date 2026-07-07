9cad8b9c09113857b46b131aeb10951bd7095967
sam su <36393679+samyilin@users.noreply.github.com>
2026-07-07 01:12:12 -0700

    FormatToggle now toggles both conform and mini.trailspace

--- nvim/nvim/plugin/mini/10_mini.trailspace.lua
+++ nvim/nvim/plugin/mini/10_mini.trailspace.lua
@@ -1,12 +1,17 @@
--- Add autocommand to trim whitespace before save. This doesn't conflict formatters, it seems.
+-- Add autocommand to trail-on-save. This doesn't conflict formatters, it seems.
 Config.later(function()
   require('mini.trailspace').setup()
+  -- Initialize the tracking variable for trail-on-save
+  vim.g.trailspace_on_save = true
+  -- Create the trim-on-save autocommand
   vim.api.nvim_create_autocmd('BufWritePre', {
     group = Config.custom_group,
     pattern = { '*' },
     callback = function()
-      MiniTrailspace.trim()
-      MiniTrailspace.trim_last_lines()
+      if vim.g.trailspace_on_save then
+        MiniTrailspace.trim()
+        MiniTrailspace.trim_last_lines()
+      end
     end,
     desc = 'Trim whitespace before save',
   })
--- nvim/nvim/plugin/other/20_final_touches.lua
+++ nvim/nvim/plugin/other/20_final_touches.lua
@@ -1,2 +1,20 @@
 -- Putting configs that need multiple plugins here for final touch.
+-- Colorscheme is determined latest, so as to ensure it's able to detect
+-- all installed colorschemes.
 vim.cmd.colorscheme('miniautumn')
+
+-- autocmd for toggling conform and mini.trailspace autoformatting.
+-- These don't touch conform or mini directly, but loading this after
+-- all plugins just in case.
+vim.api.nvim_create_user_command('FormatToggle', function()
+  vim.g.trailspace_on_save = not vim.g.trailspace_on_save
+  vim.b.disable_autoformat = not vim.b.disable_autoformat
+  vim.g.disable_autoformat = not vim.g.disable_autoformat
+  if vim.b.disable_autoformat then
+    vim.notify('Autoformat is disabled')
+  else
+    vim.notify('Autoformat is enabled')
+  end
+end, {
+  desc = 'Toggle autoformat on save',
+})
