4637fecae12d02d8b3d32e67db3ad952ec3a9a35
sam su <36393679+samyilin@users.noreply.github.com>
2026-08-03 18:11:29 -0700

    install/homebrew: check both homebrew prefixes

--- install/homebrew/setup.sh
+++ install/homebrew/setup.sh
@@ -3,11 +3,12 @@
 system="$(uname -srm)"
 case ${system%% *} in
 Darwin)
-  if [ ! -d /opt/homebrew ]; then
+  # /opt/homebrew on Apple Silicon, /usr/local/Homebrew on Intel.
+  if [ ! -d /opt/homebrew ] && [ ! -d /usr/local/Homebrew ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   else
-    printf "Homebrew is already installed, quitting"
+    printf "Homebrew is already installed, quitting\n"
   fi
   ;;
-*) printf "Not MacOS, won't install Homebrew for the user." ;;
+*) printf "Not MacOS, won't install Homebrew for the user.\n" ;;
 esac
