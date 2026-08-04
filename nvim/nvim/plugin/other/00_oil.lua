2db8a70fea71454188bc41fec4edaf141d60557f
sam su <36393679+samyilin@users.noreply.github.com>
2026-08-03 22:09:31 -0700

    nvim: fix delete_to_traash typo in oil config

--- nvim/nvim/plugin/other/00_oil.lua
+++ nvim/nvim/plugin/other/00_oil.lua
@@ -5,7 +5,7 @@ Config.now(function()
   )
   require('oil').setup({
     default_file_explorer = true,
-    delete_to_traash = true,
+    delete_to_trash = true,
     view_options = {
       show_hidden = true,
       -- This function defines what is considered a "hidden" file
