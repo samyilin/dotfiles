8469189cb3ebb15866feaee9c7459d9a96e2ffd3
sam su <36393679+samyilin@users.noreply.github.com>
2026-08-19 00:27:25 -0700

    nvim: stylua format 10_colorschemes.lua

--- nvim/nvim/plugin/other/10_colorschemes.lua
+++ nvim/nvim/plugin/other/10_colorschemes.lua
@@ -1,10 +1,7 @@
 Config.later(function()
-  vim.pack.add(
-    {
-      { src = 'https://github.com/zenbones-theme/zenbones.nvim' },
-      { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
-    },
-    { load = true }
-  )
+  vim.pack.add({
+    { src = 'https://github.com/zenbones-theme/zenbones.nvim' },
+    { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
+  }, { load = true })
   vim.g.zenbones_compat = 1
 end)
