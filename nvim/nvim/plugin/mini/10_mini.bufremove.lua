5ce21f0c16cef639feb140beb15f2c81438d49cf
sam su <36393679+samyilin@users.noreply.github.com>
2026-09-12 18:45:37 -0700

    Add <leader>bo to delete other buffers via mini.bufremove

--- nvim/nvim/plugin/mini/10_mini.bufremove.lua
+++ nvim/nvim/plugin/mini/10_mini.bufremove.lua
@@ -6,4 +6,12 @@ Config.later(function()
     function() MiniBufremove.delete() end,
     { desc = 'Buffer Delete' }
   )
+  vim.keymap.set('n', '<Leader>bo', function()
+    local cur = vim.api.nvim_get_current_buf()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if buf ~= cur and vim.fn.buflisted(buf) == 1 then
+        MiniBufremove.delete(buf)
+      end
+    end
+  end, { desc = 'Buffer Delete Others' })
 end)
